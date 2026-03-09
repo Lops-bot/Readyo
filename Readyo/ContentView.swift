901610fb1eede6328a5e6aafc9eb751ba19e057e
LSinha <sinhalopa@gmail.com>
2026-03-08 23:18:36 -0400

    Initial Commit

---
+++ Readyo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Readyo
+//
+//  Created by Lopamudra Sinha on 3/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Readyo/ReadyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadyoApp.swift
+//  Readyo
+//
+//  Created by Lopamudra Sinha on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
